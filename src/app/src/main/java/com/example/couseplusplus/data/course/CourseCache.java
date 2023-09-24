4749234d034cbca8b465165be0272e2afed7de78
Yuki Misumi <u7582380@anu.edu.au>
2023-09-24 21:58:52 +1000

    added course cache

--- src/app/src/main/java/com/example/couseplusplus/data/course/CourseCache.java
+++ src/app/src/main/java/com/example/couseplusplus/data/course/CourseCache.java
@@ -1,3 +1,24 @@
 package com.example.couseplusplus.data.course;
 
-public class CourseCache {}
+import com.example.couseplusplus.data.avl.AVLTree;
+import com.example.couseplusplus.model.course.Course;
+import java.util.Comparator;
+import java.util.List;
+
+public class CourseCache {
+  List<Course> courses;
+  AVLTree<Course> codeTree;
+  AVLTree<Course> nameTree;
+
+  public CourseCache(List<Course> courses) {
+    this.courses = courses;
+    codeTree = new AVLTree<>(Comparator.comparing(Course::code));
+    nameTree = new AVLTree<>(Comparator.comparing(Course::name));
+
+    courses.forEach(
+        course -> {
+          codeTree.insert(course);
+          nameTree.insert(course);
+        });
+  }
+}
