bdda6793598660e71c9898b231e32200003da594
Yuki Misumi <u7582380@anu.edu.au>
2023-09-24 21:53:40 +1000

    added comment cache

--- src/app/src/main/java/com/example/couseplusplus/data/comment/CommentCache.java
+++ src/app/src/main/java/com/example/couseplusplus/data/comment/CommentCache.java
@@ -1,3 +1,31 @@
 package com.example.couseplusplus.data.comment;
 
-public class CommentCache {}
+import com.example.couseplusplus.data.avl.AVLTree;
+import com.example.couseplusplus.model.comment.Comment;
+import java.util.Comparator;
+import java.util.List;
+
+public class CommentCache {
+  List<Comment> comments;
+  AVLTree<Comment> helpfulTree;
+  AVLTree<Comment> enrolTree;
+  AVLTree<Comment> postedTree;
+  AVLTree<Comment> textTree;
+
+  public CommentCache(List<Comment> comments) {
+    this.comments = comments;
+    helpfulTree = new AVLTree<>(Comparator.comparing(Comment::helpfulness));
+    enrolTree =
+        new AVLTree<>(Comparator.comparingInt(Comment::year).thenComparingInt(Comment::semester));
+    postedTree = new AVLTree<>(Comparator.comparing(Comment::postedDateTime));
+    textTree = new AVLTree<>(Comparator.comparing(Comment::text));
+
+    comments.forEach(
+        comment -> {
+          helpfulTree.insert(comment);
+          enrolTree.insert(comment);
+          postedTree.insert(comment);
+          textTree.insert(comment);
+        });
+  }
+}
