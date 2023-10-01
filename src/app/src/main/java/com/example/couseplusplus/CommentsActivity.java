f1e7d5f5c437a902022eaf6399181d0c4d660df0
porntipapoonpolsub <u7618313@anu.edu.au>
2023-10-01 21:00:44 +1100

    showed comments

--- src/app/src/main/java/com/example/couseplusplus/CommentsActivity.java
+++ src/app/src/main/java/com/example/couseplusplus/CommentsActivity.java
@@ -1,13 +1,32 @@
 package com.example.couseplusplus;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.couseplusplus.model.comment.Comment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommentsActivity extends AppCompatActivity {
+    DatabaseReference mDatabase;
+    RecyclerView commentRecycleView;
+    CommentAdapter commentAdapter;
+    List<Comment> commentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +37,33 @@ public class CommentsActivity extends AppCompatActivity {
 
         TextView courseCodeInfoTextView = findViewById(R.id.course_code_info);
         courseCodeInfoTextView.setText(courseCodeInfo);
-        ListView commentsListView = findViewById(R.id.commentsListView);
+        commentRecycleView = (RecyclerView) findViewById(R.id.comment_rv);
+        commentRecycleView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Create an ArrayAdapter or RecyclerViewAdapter to display comments.
-        // Set the adapter to the ListView.
-        // Example: commentsListView.setAdapter(adapter);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.getKey().equals("comment")) {
+                        for (DataSnapshot courseSnapshot : snapshot.getChildren()) {
+                            if (courseSnapshot.getKey().equals(courseCodeInfo)) {
+                                for (DataSnapshot commentSnapshot : courseSnapshot.getChildren()) {
+                                    Comment comment = commentSnapshot.getValue(Comment.class);
+                                    commentList.add(comment);
+                                }
+                            }
+                        }
+                    }
+                }
+                commentAdapter = new CommentAdapter(commentList);
+                commentRecycleView.setAdapter(commentAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {  // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
