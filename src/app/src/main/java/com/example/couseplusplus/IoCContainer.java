a6c1c388f18ab61a086d62ed3cc353510acb8c79
Yuki Misumi <u7582380@anu.edu.au>
2023-10-09 10:45:23 +1100

    annotated author

--- src/app/src/main/java/com/example/couseplusplus/IoCContainer.java
+++ src/app/src/main/java/com/example/couseplusplus/IoCContainer.java
@@ -4,11 +4,9 @@ import com.example.couseplusplus.data.user.UserDatasource;
 import com.example.couseplusplus.model.user.UserRepository;
 import com.example.couseplusplus.service.user.UserService;
 
-// Doing inversion of control.
-// Why not instantiating in activity? - to ensure each service class is singleton.
-// Why not inserting UserService#userRepository inside the constructor? - to avoid service layer
-// knows about data layer.
-// https://www.dotnettricks.com/learn/dependencyinjection/what-is-ioc-container-or-di-container
+/**
+ * @author Yuki Misumi (u7582380)
+ */
 public class IoCContainer {
   private static final UserRepository userRepository = new UserDatasource();
   private static final UserService userService = new UserService(userRepository);
