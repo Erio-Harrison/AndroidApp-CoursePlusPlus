db81df1438e6bca7a23f201b2ba96f8e08c0a870
Yuki Misumi <u7582380@anu.edu.au>
2023-10-09 23:51:58 +1100

    separated firebase dedicated class

--- src/app/src/main/java/com/example/couseplusplus/data/course/CourseCreator.java
+++ src/app/src/main/java/com/example/couseplusplus/data/course/CourseCreator.java
@@ -11,28 +11,4 @@ public class CourseCreator {
             snapshot.getValue(FirebaseCourse.class), "Failed to get course data");
     return new NewCourse(value.courseCode, value.courseName);
   }
-
-  private static class FirebaseCourse {
-    String courseCode;
-
-    String courseName;
-
-    public FirebaseCourse() {}
-
-    public String getCourseCode() {
-      return courseCode;
-    }
-
-    public void setCourseCode(String courseCode) {
-      this.courseCode = courseCode;
-    }
-
-    public String getCourseName() {
-      return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-      this.courseName = courseName;
-    }
-  }
 }
---
+++ src/app/src/main/java/com/example/couseplusplus/data/course/FirebaseCourse.java
@@ -0,0 +1,25 @@
+package com.example.couseplusplus.data.course;
+
+public class FirebaseCourse {
+  String courseCode;
+
+  String courseName;
+
+  public FirebaseCourse() {}
+
+  public String getCourseCode() {
+    return courseCode;
+  }
+
+  public void setCourseCode(String courseCode) {
+    this.courseCode = courseCode;
+  }
+
+  public String getCourseName() {
+    return courseName;
+  }
+
+  public void setCourseName(String courseName) {
+    this.courseName = courseName;
+  }
+}
