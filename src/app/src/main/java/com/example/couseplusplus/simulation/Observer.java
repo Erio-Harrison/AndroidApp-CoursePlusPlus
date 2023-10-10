04193959404fd26a4c26ffbdb9473bfc35d2680f
Yuki Misumi <u7582380@anu.edu.au>
2023-10-10 12:21:42 +1100

    added comment

--- src/app/src/main/java/com/example/couseplusplus/simulation/Observer.java
+++ src/app/src/main/java/com/example/couseplusplus/simulation/Observer.java
@@ -3,6 +3,8 @@ package com.example.couseplusplus.simulation;
 import java.util.List;
 
 // TODO change however you want to suite your implementation needs
+
+/** Observer pattern! Responsible for reacting to the notification from {@link Subject} */
 public interface Observer {
   void on(ActionType actionType, List<String> arguments);
 }
--- src/app/src/main/java/com/example/couseplusplus/simulation/Subject.java
+++ src/app/src/main/java/com/example/couseplusplus/simulation/Subject.java
@@ -3,6 +3,8 @@ package com.example.couseplusplus.simulation;
 import java.util.List;
 
 // TODO change however you want to suite your implementation needs
+
+/** Observer pattern! Responsible for notifying observers the event. */
 public interface Subject {
   default void emit(ActionType actionType, List<String> arguments) {
     observers().forEach(observer -> observer.on(actionType, arguments));
