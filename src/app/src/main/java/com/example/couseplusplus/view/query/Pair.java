8bbf7f349c4fdc421c087f2802dd96f88dddf457
Yuki Misumi <u7582380@anu.edu.au>
2023-10-11 22:18:37 +1100

    added comment

--- src/app/src/main/java/com/example/couseplusplus/view/query/Pair.java
+++ src/app/src/main/java/com/example/couseplusplus/view/query/Pair.java
@@ -1,5 +1,12 @@
 package com.example.couseplusplus.view.query;
 
+/**
+ * A utility for tuple
+ *
+ * @param <T>
+ * @param <U>
+ * @author Yuki Misumi (u7582380)
+ */
 public class Pair<T, U> {
   T first;
   U second;
