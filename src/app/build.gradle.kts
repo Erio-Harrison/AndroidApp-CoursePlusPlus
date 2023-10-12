fb700a29c8ca8d90215329959b91a19d8b6c05e2
Haoren <u7541840@anu.edu.au>
2023-10-13 10:49:28 +1100

    update csv.file and add test

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
